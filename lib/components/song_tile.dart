7ccca6a762ad99108590537c0ab887b343bf4206
hasithaPerera2002 <hasithaperera2017@gmail.com>
2024-02-14 00:23:05 +0530

    added songs and images using audio query library

---
+++ lib/components/song_tile.dart
@@ -0,0 +1,55 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+
+class SongTile extends StatefulWidget {
+  final SongModel song;
+  final OnAudioQuery audioQuery;
+  const SongTile({Key? key, required this.song, required this.audioQuery});
+
+  @override
+  State<SongTile> createState() => _SongTileState();
+}
+
+class _SongTileState extends State<SongTile> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        title: SizedBox(
+          height: 20,
+          child: Text(
+            widget.song.title,
+            style: const TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+        subtitle: Text(
+          widget.song.artist ?? 'Unknown Artist',
+          style: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        isThreeLine: false,
+        leading: QueryArtworkWidget(
+          id: widget.song.id,
+          type: ArtworkType.AUDIO,
+          artworkFit: BoxFit.cover,
+          controller: widget.audioQuery,
+          artworkBorder: BorderRadius.circular(10),
+          artworkHeight: 80,
+          artworkWidth: 50,
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.play_arrow),
+          onPressed: () {
+            //  widget.audioQuery.play(musicId: widget.song.id);
+          },
+        ));
+  }
+}
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,15 +1,75 @@
+// ignore_for_file: avoid_print, prefer_const_constructors, prefer_const_literals_to_create_immutables, unnecessary_null_comparison
+
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:music_app/components/song_tile.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:on_audio_query/on_audio_query.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final OnAudioQuery _audioQuery = OnAudioQuery();
+  List<SongModel> songs = [];
+
+  @override
+  void initState() {
+    super.initState();
+    permission();
+    LogConfig logConfig = LogConfig(logType: LogType.DEBUG);
+    _audioQuery.setLogConfig(logConfig);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Music App'),
+        ),
+        body: songs == null
+            ? Center(
+                child: Text('Searching For Songs'),
+              )
+            : ListView.builder(
+                itemCount: songs.length,
+                itemBuilder: (context, index) {
+                  return SongTile(
+                    song: songs[index],
+                    audioQuery: _audioQuery,
+                  );
+                },
+              ),
+      ),
+    );
+  }
+
+  Future<void> getFiles() async {
+    if (Platform.isAndroid) {
+      songs = await _audioQuery.querySongs();
+
+      setState(() {});
+    }
+  }
+
+  Future<void> permission() async {
+    final PermissionStatus status = await Permission.audio.status;
+    if (status.isDenied || status.isRestricted) {
+      final PermissionStatus newStatus = await Permission.audio.request();
+      if (newStatus.isDenied) {
+      } else if (newStatus.isGranted) {
+        getFiles();
+      }
+    } else if (status.isPermanentlyDenied) {
+      openAppSettings();
+    } else {
+      getFiles();
+    }
   }
 }
